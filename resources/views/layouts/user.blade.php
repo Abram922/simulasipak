198d2ef515d770e3b94ed25b4670f94ba4861d89
abram <abrampane922@gmail.com>
2023-04-24 19:10:23 +0700

    selasa sebelum pulang

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/universal.css') }}">
     
     <title>
         @yield('title')
@@ -12,29 +13,59 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   </head>
   <body>
-    <div class="col-md-10 mx-auto">
-      <nav class="navbar navbar-expand-lg ">
+      <div class="col-md-10 mx-auto">
+        <nav class="navbar navbar-expand-lg navbar-light">
           <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" href="#">Brand</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
               <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                <a class="nav-link" href="#">Features</a>
-                <a class="nav-link" href="#">Pricing</a>
-                <a class="nav-link disabled">Disabled</a>
-              </div>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Perhitungan </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+              </ul>
+              <ul class="navbar-nav ms-auto">
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Link
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                </li>
+              </ul>
             </div>
           </div>
-      </nav>
-    </div>
+        </nav>
+        
 
-    <div class="col-md-10 mx-auto">
-      @yield('content1')
-      @yield('content2')
-    </div>
+        @yield('content1')
+
+      </div >
+
+      </div class="col-md-10 mx-auto">
+          
+          @yield('content2')
+      <div >
+
+      
+
+      </div>
+
+
+      
+   
     
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,6 +1,70 @@
 @extends('.layouts.user')
 
 @section('content1')
+    <div class="card rounded-3 text-black border-0 ">
+      <div class="row g-0">
+        <div class="col-lg-7">
+            <div class="card-body p-md-8 mx-md-4">
+                <form >
+                    <div class="row my-2 no-gutters">
+                        <div class="col">
+                            <div class="card m-3" style="background-color: rgb(0, 51, 124, 0.3)">
+                                <div class="card-body d-flex justify-content-between ">
+                                    <button type="button" class="btn " style="background-color:white" >Button</button>
+                                    <p class="card-text">Pendidikan dan Pengajaran</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="card m-3" style="background-color: rgb(0, 51, 124, 0.3)">
+                                <div class="card-body d-flex justify-content-between ">
+                                    <button type="button" class="btn " style="background-color:white">Button</button>
+                                    <p class="card-text">Penelitian</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row my-2 no-gutters">
+                        <div class="col">
+                            <div class="card m-3" style="background-color: rgb(0, 51, 124, 0.3)">
+                                <div class="card-body d-flex justify-content-between ">
+                                    <button type="button" class="btn"style="background-color:white">Button</button>
+                                    <p class="card-text">Pengabdian Masyarakat</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="card m-3" style="background-color: rgb(0, 51, 124, 0.3)">
+                                <div class="card-body d-flex justify-content-between ">
+                                    <button type="button" class="btn"style="background-color:white">Button</button>
+                                    <p class="card-text">Dokumen Penunjang</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+        <div class="col-lg-2 d-flex align-items-center ">
+            <div class="card">
+                <div class="card-body d-flex" id="cardprofil">
+                  <img id="cardprofil-img" src="{{ asset('/aset_web/p1.png') }}" alt="Profile Picture" class="mr-3">
+                  <div id="cardprofil-img">
+                    <h5 class="card-title">Nama Pengguna</h5>
+                    <p class="card-text">Deskripsi profil pengguna</p>
+                  </div>
+                </div>
+            </div>
+              
+        </div>
+        </div>
+      </div>
+    </div>
+
 
     
 @endsection
