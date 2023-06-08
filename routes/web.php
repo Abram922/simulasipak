321d817b2ad3202bfc71913edaddc76ee99bd665
abram <abrampane922@gmail.com>
2023-06-08 14:44:31 +0700

    fix31

--- routes/web.php
+++ routes/web.php
@@ -29,37 +29,37 @@ Route::get('/', function () { return view('auth/login');})->name('/halaman_login
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/user-home/{id}', [App\Http\Controllers\HomeController::class, 'userhomex'])->name('user.homex');
-Route::get('/user-home', [App\Http\Controllers\HomeController::class, 'userhome'])->name('userhome');
-Route::get('/admin-home', [App\Http\Controllers\HomeController::class,'adminhome'])->name('adminhome');
-Route::get('/board-pak', [App\Http\Controllers\HomeController::class,'boardpak'])->name('boardpak');
-Route::get('/profil', [App\Http\Controllers\ProfilController::class, 'index'])->name('profil');
-Route::get('/ubahprofil/{id}', [App\Http\Controllers\ProfilController::class, 'edit'])->name('profill');
-Route::put('/ubahprofil/update/{id}', [App\Http\Controllers\ProfilController::class, 'update'])->name('profilll');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/user-home/{id}', [App\Http\Controllers\HomeController::class, 'userhomex'])->name('user.homex');
+// Route::get('/user-home', [App\Http\Controllers\HomeController::class, 'userhome'])->name('userhome');
+// Route::get('/admin-home', [App\Http\Controllers\HomeController::class,'adminhome'])->name('adminhome');
+// Route::get('/board-pak', [App\Http\Controllers\HomeController::class,'boardpak'])->name('boardpak');
+// Route::get('/profil', [App\Http\Controllers\ProfilController::class, 'index'])->name('profil');
+// Route::get('/ubahprofil/{id}', [App\Http\Controllers\ProfilController::class, 'edit'])->name('profill');
+// Route::put('/ubahprofil/update/{id}', [App\Http\Controllers\ProfilController::class, 'update'])->name('profilll');
 
-Route::resource('kum', App\Http\Controllers\KumController::class);
-Route::resource('pendidikan', App\Http\Controllers\PendidikanController::class);
-Route::resource('pelaksanaanpendidikan', App\Http\Controllers\PelaksanaanPendidikanController::class);
-Route::resource('pelaksanaanpenelitian', App\Http\Controllers\PelaksananPenelitianController::class);
-Route::resource('pelaksanaan_pm', App\Http\Controllers\PelaksanaanPmController::class);
-Route::resource('unsurdp', App\Http\Controllers\DokumenpenunjangController::class);
-Route::resource('doumenpenunjang', App\Http\Controllers\DokumenpenunjangController::class);
-Route::resource('pengajaran', App\Http\Controllers\PengajaranController::class);
-Route::resource('karya', App\Http\Controllers\PenelitianHakidankaryaController::class);
+// Route::resource('kum', App\Http\Controllers\KumController::class);
+// Route::resource('pendidikan', App\Http\Controllers\PendidikanController::class);
+// Route::resource('pelaksanaanpendidikan', App\Http\Controllers\PelaksanaanPendidikanController::class);
+// Route::resource('pelaksanaanpenelitian', App\Http\Controllers\PelaksananPenelitianController::class);
+// Route::resource('pelaksanaan_pm', App\Http\Controllers\PelaksanaanPmController::class);
+// Route::resource('unsurdp', App\Http\Controllers\DokumenpenunjangController::class);
+// Route::resource('doumenpenunjang', App\Http\Controllers\DokumenpenunjangController::class);
+// Route::resource('pengajaran', App\Http\Controllers\PengajaranController::class);
+// Route::resource('karya', App\Http\Controllers\PenelitianHakidankaryaController::class);
 
-Route::resource('unsurpelaksanaan', App\Http\Controllers\PelaksanaanPendidikanController::class);
-Route::resource('semester', App\Http\Controllers\SemesterController::class);
-Route::resource('akreditasipenelitian', App\Http\Controllers\AkreditasiPenelitianController::class);
-Route::resource('penulis', App\Http\Controllers\PenulisController::class);
-Route::resource('jabatan', App\Http\Controllers\JabatanController::class);
+// Route::resource('unsurpelaksanaan', App\Http\Controllers\PelaksanaanPendidikanController::class);
+// Route::resource('semester', App\Http\Controllers\SemesterController::class);
+// Route::resource('akreditasipenelitian', App\Http\Controllers\AkreditasiPenelitianController::class);
+// Route::resource('penulis', App\Http\Controllers\PenulisController::class);
+// Route::resource('jabatan', App\Http\Controllers\JabatanController::class);
 
 
-Route::get('/lampiran', [App\Http\Controllers\LampiranController::class,'index'])->name('lampiran');
-Route::get('/lampiran-datapenelitian', [App\Http\Controllers\LampiranController::class,'datapenelitian'])->name('datapenelitian');
-Route::get('/lampiran-datapenunjang', [App\Http\Controllers\LampiranController::class,'datapenunjang'])->name('datapenunjang');
-Route::get('/lampiran-datapendidikan', [App\Http\Controllers\LampiranController::class,'datapendidikan'])->name('lampirandatapendidikan');
-Route::get('/lampiran-datapm', [App\Http\Controllers\LampiranController::class,'datapm'])->name('datapm');
+// Route::get('/lampiran', [App\Http\Controllers\LampiranController::class,'index'])->name('lampiran');
+// Route::get('/lampiran-datapenelitian', [App\Http\Controllers\LampiranController::class,'datapenelitian'])->name('datapenelitian');
+// Route::get('/lampiran-datapenunjang', [App\Http\Controllers\LampiranController::class,'datapenunjang'])->name('datapenunjang');
+// Route::get('/lampiran-datapendidikan', [App\Http\Controllers\LampiranController::class,'datapendidikan'])->name('lampirandatapendidikan');
+// Route::get('/lampiran-datapm', [App\Http\Controllers\LampiranController::class,'datapm'])->name('datapm');
 
 
 
